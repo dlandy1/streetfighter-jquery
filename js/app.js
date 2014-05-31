7f2352df992ecec46610cdc7ef601e4f4b8ddc2d
dlandy1 <DanLandy@Daniels-MacBook-Pro-6.local>
2014-05-31 01:30:42 -0400

    Ryu complete

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
   .mousedown(function() {
     playHadouken(); 
     $('.ryu-ready').hide();
+    $('.text').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     	.animate(
@@ -18,16 +19,49 @@ $(document).ready(function() {
 		  function() {
 	    	$(this).hide();
 	    	$(this).css('left', '-212px');
-	  }
-	);
-  })
+	    }
+	    );
+	})
   .mouseup(function() {
     $('.ryu-throwing').hide();
   	$('.ryu-ready').show();
+  	$('.text').show();
   });
-})
+});
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+$(document).keydown(function(event) {
+   	if(event.keyCode == 88) {
+   			$('.ryu-cool').show();
+   			$('.ryu-ready').hide();
+   			$('ryu-still').hide();
+   	}
+   })
+$(document).keyup(function(event){
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+			})
+			  .mousedown(function() {
+			    playHadouken(); 
+			    $('.ryu-ready').hide();
+			    $('.ryu-still').hide();
+			    $('.text').hide();
+			    $('.ryu-throwing').show();
+			    $('.hadouken').finish().show()
+				    .animate(
+					  {'left': '300px'},
+					  500,
+					  function() {
+				    	$(this).hide();
+				    	$(this).css('left', '-212px');
+				    }
+				    );
+					})
+				  .mouseup(function() {
+				    $('.ryu-throwing').hide();
+				    $('.text').show();
+				  	$('.ryu-ready').show();
+				  });
